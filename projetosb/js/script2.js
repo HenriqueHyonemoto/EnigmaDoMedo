9ed0ed50b2ff35e1fd7ab15494db93e7f3f84132
HenriqueHyonemoto <ifhenriquehyonemoto@gmail.com>
2025-06-24 14:30:43 -0300

    tabela

---
+++ projetosb/js/script2.js
@@ -0,0 +1,24 @@
+
+const audio = document.createElement('audio');
+audio.src = '../sound/HeilagVagga.mp3';
+audio.type = 'audio/mpeg';
+audio.loop = true;
+audio.autoplay = true;
+audio.volume = 0.65;
+
+window.addEventListener('load', () => {
+  document.body.appendChild(audio);
+
+
+  audio.play().catch(() => {
+    const iniciarSom = () => {
+      audio.play().then(() => {
+        window.removeEventListener('click', iniciarSom);
+        window.removeEventListener('scroll', iniciarSom);
+      });
+    };
+
+    window.addEventListener('click', iniciarSom);
+    window.addEventListener('scroll', iniciarSom);
+  });
+});
